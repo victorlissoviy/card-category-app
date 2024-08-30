9a8598dc658b8125aeafe76cd64f4b6f8082c06d
Лісовий Віктор <victor.lissoviy.93@gmail.com>
2024-08-30 20:57:03 +0300

    update wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,24 @@
 plugins {
-    id("java")
+  id("java")
 }
 
 group = "org.home.lissoviy"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation(platform("org.junit:junit-bom:5.10.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
+}
+
+tasks.wrapper {
+  version = "latest"
+  distributionType = Wrapper.DistributionType.ALL
 }
