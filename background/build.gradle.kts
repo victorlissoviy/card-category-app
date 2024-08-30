b77dd24c701956dcdd70ec11b9322e3ea800e4f5
Лісовий Віктор <victor.lissoviy.93@gmail.com>
2024-08-30 21:02:08 +0300

    add empty background

---
+++ background/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+  java
+  id("org.springframework.boot") version "3.3.3"
+  id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "org.home.lissoviy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(22)
+  }
+}
+
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  compileOnly("org.projectlombok:lombok")
+  developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+  runtimeOnly("org.postgresql:postgresql")
+  annotationProcessor("org.projectlombok:lombok")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+  testImplementation("org.springframework.security:spring-security-test")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
---
+++ background/src/test/java/org/home/lissoviy/background/BackgroundApplicationTests.java
@@ -0,0 +1,14 @@
+package org.home.lissoviy.background;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroundApplicationTests {
+
+  @Test
+  void contextLoads() {
+    //empty block
+  }
+
+}
