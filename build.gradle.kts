570338b1bd59b42824374535f67dd54b464ba766
Лісовий Віктор <victor.lissoviy.93@gmail.com>
2024-09-03 06:19:35 +0300

    remove module background

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,71 @@
 plugins {
-  id("java")
+  java
+  id("org.springframework.boot") version "3.3.3"
+  id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "org.home.lissoviy"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
+
+var mapstructVersion = "1.6.0"
+var collections4Version = "4.4"
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(22)
+  }
+
+}
+
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
 
 repositories {
   mavenCentral()
 }
 
 dependencies {
-  testImplementation(platform("org.junit:junit-bom:5.10.0"))
-  testImplementation("org.junit.jupiter:junit-jupiter")
+  implementation("org.springframework.boot:spring-boot-starter-security")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.mapstruct:mapstruct:$mapstructVersion")
+  implementation("org.apache.commons:commons-collections4:$collections4Version")
+
+  compileOnly("org.projectlombok:lombok")
+
+  runtimeOnly("org.postgresql:postgresql")
+
+  annotationProcessor("org.projectlombok:lombok")
+  annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+
+  testImplementation("org.mapstruct:mapstruct:$mapstructVersion")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+  testAnnotationProcessor("org.projectlombok:lombok")
+  testAnnotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
 }
 
-tasks.test {
+tasks.withType<Test> {
   useJUnitPlatform()
 }
 
 tasks.wrapper {
   version = "latest"
   distributionType = Wrapper.DistributionType.ALL
+}
+
+// Needs for show mapping information when start
+tasks.named<JavaCompile>("compileJava") {
+  options.compilerArgs.addAll(
+    listOf(
+      "-Amapstruct.suppressGeneratorTimestamp=true",
+      "-Amapstruct.suppressGeneratorVersionInfoComment=true",
+      "-Amapstruct.verbose=true"
+    )
+  )
 }
