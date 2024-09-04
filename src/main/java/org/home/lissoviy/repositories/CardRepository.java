cfb077fffbfbde10d2ff7e90d11630916a08a05d
Лісовий Віктор <victor.lissoviy.93@gmail.com>
2024-09-04 14:36:15 +0300

    modify CardRepository for get cards by contains categories

--- src/main/java/org/home/lissoviy/repositories/CardRepository.java
+++ src/main/java/org/home/lissoviy/repositories/CardRepository.java
@@ -2,9 +2,14 @@ package org.home.lissoviy.repositories;
 
 
 import org.home.lissoviy.models.Card;
+import org.home.lissoviy.models.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardRepository extends CrudRepository<Card, Long> {
+
+  List<Card> findAllByCategoriesContains(Category category);
 }
